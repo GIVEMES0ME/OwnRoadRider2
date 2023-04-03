6d54e987c0c461008b84b5cba1e8c5061777660d
KimMinSeop <kimsman1031@Naver.com>
2023-04-03 15:14:03 +0900

    [UPDATE] 마이페이지로 이동 및 마이페이지 버튼에 토스트 메시지 출력 기능 추가 (-민섭)

--- app/src/main/java/com/example/ownroadrider/MainActivity.java
+++ app/src/main/java/com/example/ownroadrider/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.ownroadrider;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int Fragment_mypage = 1;
     private ImageButton map;
     private Button theme1;
+    private ImageButton myPageBtn;
+    private ImageButton homeBtn;
+    private FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
         map = findViewById(R.id.mapButton);
         theme1 = findViewById(R.id.themeButton1);
+        myPageBtn = findViewById(R.id.mypageButton);
+        homeBtn = findViewById(R.id.homeButton);
+        fragmentContainer = findViewById(R.id.fragment_container);
 
+        MyPageFragment mypageFragment = new MyPageFragment();
         map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,6 +52,22 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"테마선택입니다",Toast.LENGTH_LONG).show();
             }
         });
-    }
 
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"메인화면입니다.",Toast.LENGTH_LONG).show();
+                fragmentContainer.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        myPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"마이페이지입니다.",Toast.LENGTH_LONG).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, mypageFragment).commitAllowingStateLoss();
+                fragmentContainer.setVisibility(View.VISIBLE);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/ownroadrider/MyPageFragment.java
+++ app/src/main/java/com/example/ownroadrider/MyPageFragment.java
@@ -1,64 +1,69 @@
 package com.example.ownroadrider;
 
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyPageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 public class MyPageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ImageButton settingBtn;
+    Button manageReviewBtn;
+    Button checkedDestBtn;
+    Button listDestBtn;
+    Button recommendScheduleBtn;
+    Button.OnClickListener clickListener;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MyPageFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyPageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyPageFragment newInstance(String param1, String param2) {
-        MyPageFragment fragment = new MyPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my_page,container,false);
+        settingBtn = (ImageButton) view.findViewById(R.id.settingBtn);
+        manageReviewBtn = (Button) view.findViewById(R.id.manageReviewBtn);
+        checkedDestBtn = (Button) view.findViewById(R.id.checkedDestBtn);
+        listDestBtn = (Button) view.findViewById(R.id.listDestBtn);
+        recommendScheduleBtn = (Button) view.findViewById(R.id.recommendScheduleBtn);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false);
+        clickListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.settingBtn:
+                        Toast.makeText(getContext(),"설정 버튼",Toast.LENGTH_SHORT).show();
+                        Log.d(this.getClass().getName(), "설정 버튼");
+                        break;
+                    case R.id.manageReviewBtn:
+                        Toast.makeText(getContext(),"나의 리뷰 관리",Toast.LENGTH_SHORT).show();
+                        Log.d(this.getClass().getName(), "나의 리뷰 관리");                        break;
+                    case R.id.checkedDestBtn:
+                        Toast.makeText(getContext(),"내가 본 여행지",Toast.LENGTH_SHORT).show();
+                        Log.d(this.getClass().getName(), "내가 본 여행지");                        break;
+                    case R.id.listDestBtn:
+                        Toast.makeText(getContext(),"관심 여행지 목록",Toast.LENGTH_SHORT).show();
+                        Log.d(this.getClass().getName(), "관심 여행지 목록");                        break;
+                    case R.id.recommendScheduleBtn:
+                        Toast.makeText(getContext(),"여행 고수들의 추천 일정!",Toast.LENGTH_SHORT).show();
+                        Log.d(this.getClass().getName(), "여행 고수들의 추천 일정!");                        break;
+                }
+            }
+        };
+
+        settingBtn.setOnClickListener(clickListener);
+        manageReviewBtn.setOnClickListener(clickListener);
+        checkedDestBtn.setOnClickListener(clickListener);
+        listDestBtn.setOnClickListener(clickListener);
+        recommendScheduleBtn.setOnClickListener(clickListener);
+
+        return view;
     }
 }
